8d67573da4a41ddd59ea1c9c0cb8db964567ce08
Adrian Conlin <adrian.conlin@gmail.com>
2015-12-12 17:20:08 +0000

    Add tslint.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,23 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
+		pkg: grunt.file.readJSON("package.json"),
+
+		tslint: {
+			options: {
+				configuration: "tslint.json"
+			},
+			app: {
+				src: [
+					"src/app/**/*.ts"
+				]
+			}
+		}
 	});
+
+	grunt.loadNpmTasks("grunt-tslint");
+
+	// Default task(s).
+	grunt.registerTask("default", ["tslint:app"]);
 
 };
